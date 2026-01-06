2bf8d172274f6e14d4bf4da3744144bfc2d5eaee
Guachoo <edwjose19@gmail.com>
2026-01-06 15:30:12 -0400

    Actualizar datos de empresa GRU con información correcta

--- src/config/company.ts
+++ src/config/company.ts
@@ -3,10 +3,10 @@
 
 export const COMPANY_CONFIG = {
   razonSocial: 'GRU CORPORACIÓN NAUTICA DE SERVICIOS, C.A.',
-  rif: 'J-50725064-4',
-  domicilioFiscal: 'AV LA ESTANCIA CON CALLE ERNESTO BLOHM CC CIUDAD TAMANACO. PIRAMIDE INVERTIDA NIVEL 6 OF 610 URB CHUAO CARACAS (CHACAO) MIRANDA ZONA POSTAL 1060',
-  telefonos: '+58-212-1234567 / +58-414-9876543',
-  email: 'grunautica@gmail.com',
+  rif: 'J-507250644',
+  domicilioFiscal: 'AV LA ESTANCIA CON CALLE ERNESTO BLOHM CC CIUDAD TAMANACO, PIRAMIDE INVERTIDA NIVEL 6 OF 610 URB CHUAO CARACAS (CHACAO) MIRANDA ZONA POSTAL 1060',
+  telefonos: '+58 422 700 0477 / 0212 959 0954',
+  email: 'Grunautica@gmail.com',
   condicionesVenta: 'Pago de contado. Precios incluyen IVA. Válido por 30 días.',
 } as const;
 
